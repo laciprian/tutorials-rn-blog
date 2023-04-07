06272f865fce8fa87cded40268ba2882cd5a3944
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-04-07 17:08:31 +0300

    Add delete icon

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,7 +4,10 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
     switch (action.type) {
         case 'add_blogpost':
-            return [...state, {title: `Blog Post #${state.length + 1}`}]
+            return [
+                ...state,
+                {id: Math.floor(Math.random() * 9999), title: `Blog Post #${state.length + 1}`}
+            ]
     }
 
     return state;
@@ -12,7 +15,7 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
     return () => {
-        dispatch({ type: 'add_blogpost' });
+        dispatch({type: 'add_blogpost'});
     }
 }
 
--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,28 +1,46 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet, FlatList, Button} from "react-native";
+import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from "react-native";
 import {Context} from "../context/BlogContext";
+import {Feather} from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const {state, addBlogPost} = useContext(Context);
     return (
         <View>
-            <Text>Index Screen</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
                     return (
-                        <View>
-                            <Text>{item.title}</Text>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => console.log(item.id)}>
+                                <Feather style={styles.icon} name="trash"/>
+                            </TouchableOpacity>
                         </View>
                     )
                 }}
-            />
+                />
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+    const styles = StyleSheet.create({
+        row: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingVertical: 20,
+            paddingHorizontal: 10,
+            borderTopWidth: 1,
+            borderColor: 'grey'
+        },
+        title: {
+            fontSize: 18
+        },
+        icon: {
+            fontSize: 24
+        }
+    });
 
-export default IndexScreen;
+    export default IndexScreen;
