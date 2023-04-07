7953d10850ad37ed7c86310527426cd35a89a3a3
Ciprian Lazar <ciprian.lazar@ivfuture.uk>
2023-04-07 16:46:26 +0300

    Initial commit 2

---
+++ App.js
@@ -0,0 +1,24 @@
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import IndexScreen from "./src/screens/indexScreen";
+import {Provider} from "./src/context/BlogContext";
+import React from 'react';
+
+const navigator = createStackNavigator({
+    Index: IndexScreen
+}, {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+        title: 'Blogs'
+    }
+});
+
+const App = createAppContainer(navigator);
+
+export default () => {
+    return (
+        <Provider>
+            <App/>
+        </Provider>
+    );
+}
